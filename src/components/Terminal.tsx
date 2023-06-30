5dcc89d80bc067a05025250f4f398f48ffb708be
reyanshkharga <reyansh.kharga@gmail.com>
2023-06-30 23:47:42 +0530

    deployed

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -39,7 +39,7 @@ export const commands: Command = [
   // { cmd: "projects", desc: "view projects that I've coded", tab: 5 },
   // { cmd: "pwd", desc: "print current working directory", tab: 10 },
   { cmd: "themes", desc: "check available themes", tab: 7 },
-  { cmd: "welcome", desc: "display hero section", tab: 6 },
+  { cmd: "welcome", desc: "display welcome message", tab: 6 },
   { cmd: "history", desc: "view command history", tab: 6 },
   { cmd: "whoami", desc: "about current user", tab: 7 },
 ];
