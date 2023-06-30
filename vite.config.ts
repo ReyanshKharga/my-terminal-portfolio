1881483c8aabd6e95b709d4a656823604820b655
reyanshkharga <reyansh.kharga@gmail.com>
2023-06-30 22:26:02 +0530

    done2

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-terminal-portfolio/",
   plugins: [
     react(),
     VitePWA({
